eddfb75091babf46c2b7be696727bc5c3436db8f
Matthew Weier O'Phinney <matthew@zend.com>
2018-12-12 11:44:09 -0600

    Final review of ToFloat filter
    - Updates docblocks to follow new conventions.
    - Removes unnecessary casting of `$value` to a string.
    - Refactors `testBasic` to accept a data provider, and expands the
      values to include other scalars.
    - Provides names for all data provider items.

--- src/ToFloat.php
+++ src/ToFloat.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-filter for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-filter/blob/master/LICENSE.md New BSD License
  */
 
 namespace Zend\Filter;
@@ -18,7 +16,7 @@ class ToFloat extends AbstractFilter
      *
      * If the value provided is non-scalar, the value will remain unfiltered
      *
-     * @param  string $value
+     * @param  scalar $value
      * @return float|mixed
      */
     public function filter($value)
@@ -26,7 +24,6 @@ class ToFloat extends AbstractFilter
         if (! is_scalar($value)) {
             return $value;
         }
-        $value = (string) $value;
 
         return (float) floatval($value);
     }
--- test/ToFloatTest.php
+++ test/ToFloatTest.php
@@ -1,62 +1,68 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-filter for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-filter/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Filter;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
 use Zend\Filter\ToFloat as ToFloatFilter;
 
 class ToFloatTest extends TestCase
 {
-    /**
-     * Ensures that the filter follows expected behavior
-     *
-     * @return void
-     */
-    public function testBasic()
-    {
-        $filter = new ToFloatFilter();
-
-        $valuesExpected = [
-            'string' => 0,
-            '1'      => 1,
-            '-1'     => -1,
-            '1.1'    => 1.1,
-            '-1.1'   => -1.1,
-            '0.9'    => 0.9,
-            '-0.9'   => -0.9
-            ];
-        foreach ($valuesExpected as $input => $output) {
-            $this->assertEquals($output, $filter($input));
-        }
-    }
-
-    public function returnUnfilteredDataProvider()
+    public function filterableValuesProvider()
     {
         return [
-            [null],
-            [new \stdClass()],
-            [[
-                '1',
-                -1
-            ]]
+            'string word' => ['string', 0],
+            'string 1'    => ['1', 1],
+            'string -1'   => ['-1', -1],
+            'string 1.1'  => ['1.1', 1.1],
+            'string -1.1' => ['-1.1', -1.1],
+            'string 0.9'  => ['0.9', 0.9],
+            'string -0.9' => ['-0.9', -0.9],
+            'integer 1'   => [1, 1.0],
+            'integer -1'  => [-1, -1.0],
+            'true'        => [true, 1.0],
+            'false'       => [false, 0.0],
+            'float 1.1'   => [1.1, 1.1],
         ];
     }
 
     /**
-     * @dataProvider returnUnfilteredDataProvider
-     * @return void
+     * Ensures that the filter follows expected behavior
+     *
+     * @dataProvider filterableValuesProvider
+     * @param mixed $input
+     * @param string $expectedOutput
      */
-    public function testReturnUnfiltered($input)
+    public function testCanFilterScalarValuesAsExpected($input, $expectedOutput)
     {
         $filter = new ToFloatFilter();
+        $this->assertEquals($expectedOutput, $filter($input));
+    }
 
+    public function unfilterableValuesProvider()
+    {
+        return [
+            'null'   => [null],
+            'array'  => [[
+                '1',
+                -1,
+            ]],
+            'object' => [new stdClass()],
+        ];
+    }
+
+    /**
+     * @dataProvider unfilterableValuesProvider
+     * @param mixed $input
+     */
+    public function testReturnsUnfilterableInputVerbatim($input)
+    {
+        $filter = new ToFloatFilter();
         $this->assertEquals($input, $filter($input));
     }
 }
