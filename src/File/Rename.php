7a8c7ba94004366aa8d6ba5d8754ab4c7ee21e6e
webimpress <contact@webimpress.com>
2019-08-16 20:29:28 +0200

    Add 2nd param to uniqid function call

--- src/File/Rename.php
+++ src/File/Rename.php
@@ -329,7 +329,7 @@ class Rename extends Filter\AbstractFilter
         if ($rename['randomize']) {
             $info = pathinfo($rename['target']);
             $newTarget = $info['dirname'] . DIRECTORY_SEPARATOR .
-                $info['filename'] . uniqid('_');
+                $info['filename'] . uniqid('_', true);
             if (isset($info['extension'])) {
                 $newTarget .= '.' . $info['extension'];
             }
