aec865541ba015e9121baee875958d98ae3dd046
webimpress <contact@webimpress.com>
2019-08-16 20:27:32 +0200

    Use static callbacks
    micro-optimisation

--- src/Word/SeparatorToCamelCase.php
+++ src/Word/SeparatorToCamelCase.php
@@ -35,21 +35,25 @@ class SeparatorToCamelCase extends AbstractSeparator
             ];
             if (! extension_loaded('mbstring')) {
                 $replacements = [
-                    function ($matches) {
+                    // @codingStandardsIgnoreStart
+                    static function ($matches) {
                         return strtoupper($matches[2]);
                     },
-                    function ($matches) {
+                    static function ($matches) {
                         return strtoupper($matches[1]);
                     },
+                    // @codingStandardsIgnoreEnd
                 ];
             } else {
                 $replacements = [
-                    function ($matches) {
+                    // @codingStandardsIgnoreStart
+                    static function ($matches) {
                         return mb_strtoupper($matches[2], 'UTF-8');
                     },
-                    function ($matches) {
+                    static function ($matches) {
                         return mb_strtoupper($matches[1], 'UTF-8');
                     },
+                    // @codingStandardsIgnoreEnd
                 ];
             }
         } else {
@@ -58,12 +62,14 @@ class SeparatorToCamelCase extends AbstractSeparator
                 '#(^[\S]{1})#',
             ];
             $replacements = [
-                function ($matches) {
+                // @codingStandardsIgnoreStart
+                static function ($matches) {
                     return strtoupper($matches[2]);
                 },
-                function ($matches) {
+                static function ($matches) {
                     return strtoupper($matches[1]);
                 },
+                // @codingStandardsIgnoreEnd
             ];
         }
 
--- src/Word/UnderscoreToStudlyCase.php
+++ src/Word/UnderscoreToStudlyCase.php
@@ -22,7 +22,7 @@ class UnderscoreToStudlyCase extends UnderscoreToCamelCase
         $lowerCaseFirst = 'lcfirst';
 
         if (StringUtils::hasPcreUnicodeSupport() && extension_loaded('mbstring')) {
-            $lowerCaseFirst = function ($value) {
+            $lowerCaseFirst = static function ($value) {
                 if (0 === mb_strlen($value)) {
                     return $value;
                 }
