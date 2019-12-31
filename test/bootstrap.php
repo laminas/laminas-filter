ad4097d4c5ccdabf964f1f86a4cc2015a6a3b1c5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:53:04 -0600

    Rewriting as Laminas Project package

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-filter for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-filter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-filter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-filter/blob/master/LICENSE.md New BSD License
  */
 
 /*
