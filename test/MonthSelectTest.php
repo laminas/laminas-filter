23b62c51a9ed426db7956388f42c9eb9675be61a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:53:23 -0600

    Rewriting as Laminas Project package

--- test/MonthSelectTest.php
+++ test/MonthSelectTest.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-filter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-filter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-filter/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Filter;
+namespace LaminasTest\Filter;
 
-use Zend\Filter\MonthSelect as MonthSelectFilter;
+use Laminas\Filter\MonthSelect as MonthSelectFilter;
 
 class MonthSelectTest extends \PHPUnit_Framework_TestCase
 {
@@ -38,7 +37,7 @@ class MonthSelectTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException \Zend\Filter\Exception\RuntimeException
+     * @expectedException \Laminas\Filter\Exception\RuntimeException
      */
     public function testInvalidInput()
     {
