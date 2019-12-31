b2cc77668aff5e47f13af83afce9d8b6b576789d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:53:56 -0600

    Rewriting as Laminas Project package

--- src/Word/SeparatorToSeparator.php
+++ src/Word/SeparatorToSeparator.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-filter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-filter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-filter/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Filter\Word;
+namespace Laminas\Filter\Word;
 
-use Zend\Filter\AbstractFilter;
-use Zend\Filter\Exception;
+use Laminas\Filter\AbstractFilter;
+use Laminas\Filter\Exception;
 
 class SeparatorToSeparator extends AbstractFilter
 {
@@ -74,7 +73,7 @@ class SeparatorToSeparator extends AbstractFilter
     }
 
     /**
-     * Defined by Zend\Filter\Filter
+     * Defined by Laminas\Filter\Filter
      *
      * Returns the string $value, replacing the searched separators with the defined ones
      *
