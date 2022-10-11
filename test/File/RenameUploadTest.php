748b1418f547579082709bbdcafe9cd788223136
George Steel <george@net-glue.co.uk>
2022-10-11 10:53:28 +0100

    Remove `prophecy` in favour of native PHPUnit mocks
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/File/RenameUploadTest.php
+++ test/File/RenameUploadTest.php
@@ -7,16 +7,12 @@ namespace LaminasTest\Filter\File;
 use Laminas\Filter\Exception;
 use Laminas\Filter\File\RenameUpload as FileRenameUpload;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UploadedFileFactoryInterface;
 use Psr\Http\Message\UploadedFileInterface;
-use RuntimeException;
 use stdClass;
 
-use function array_shift;
 use function basename;
 use function copy;
 use function glob;
@@ -37,42 +33,30 @@ use const UPLOAD_ERR_OK;
 
 class RenameUploadTest extends TestCase
 {
-    use ProphecyTrait;
-
     /**
      * Path to test files
-     *
-     * @var string
      */
-    protected $filesPath;
+    private string $filesPath;
 
     /**
-     * Testfile
-     *
-     * @var string
+     * Test file
      */
-    protected $sourceFile;
+    private string $sourceFile;
 
     /**
-     * Testfile
-     *
-     * @var string
+     * Test file
      */
-    protected $targetFile;
+    private string $targetFile;
 
     /**
-     * Testdirectory
-     *
-     * @var string
+     * Test directory
      */
-    protected $targetPath;
+    private string $targetPath;
 
     /**
-     * Testfile in Testdirectory
-     *
-     * @var string
+     * Test file in Test directory
      */
-    protected $targetPathFile;
+    private string $targetPathFile;
 
     /**
      * Sets the path to test files
@@ -99,7 +83,7 @@ class RenameUploadTest extends TestCase
         $this->removeDir($this->filesPath);
     }
 
-    protected function removeDir($dir)
+    private function removeDir(string $dir)
     {
         if (! is_dir($dir)) {
             return;
@@ -112,7 +96,6 @@ class RenameUploadTest extends TestCase
             }
             if (is_dir($file)) {
                 $this->removeDir($file);
-                continue;
             }
         }
 
@@ -125,20 +108,20 @@ class RenameUploadTest extends TestCase
     public function testThrowsExceptionWithNonUploadedFile(): void
     {
         $filter = new FileRenameUpload($this->targetFile);
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertSame('falsefile', $filter('falsefile'));
         $this->expectException(Exception\RuntimeException::class);
         $this->expectExceptionMessage('could not be renamed');
-        $this->assertSame($this->targetFile, $filter($this->sourceFile));
+        self::assertSame($this->targetFile, $filter($this->sourceFile));
     }
 
     public function testOptions(): void
     {
         $filter = new FileRenameUpload($this->targetFile);
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertFalse($filter->getUseUploadName());
-        $this->assertFalse($filter->getOverwrite());
-        $this->assertFalse($filter->getRandomize());
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertFalse($filter->getUseUploadName());
+        self::assertFalse($filter->getOverwrite());
+        self::assertFalse($filter->getRandomize());
 
         $filter = new FileRenameUpload([
             'target'          => $this->sourceFile,
@@ -146,25 +129,25 @@ class RenameUploadTest extends TestCase
             'overwrite'       => true,
             'randomize'       => true,
         ]);
-        $this->assertSame($this->sourceFile, $filter->getTarget());
-        $this->assertTrue($filter->getUseUploadName());
-        $this->assertTrue($filter->getOverwrite());
-        $this->assertTrue($filter->getRandomize());
+        self::assertSame($this->sourceFile, $filter->getTarget());
+        self::assertTrue($filter->getUseUploadName());
+        self::assertTrue($filter->getOverwrite());
+        self::assertTrue($filter->getRandomize());
     }
 
     public function testStringConstructorParam(): void
     {
         $filter = new RenameUploadMock($this->targetFile);
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertSame($this->targetFile, $filter($this->sourceFile));
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertSame($this->targetFile, $filter($this->sourceFile));
+        self::assertSame('falsefile', $filter('falsefile'));
     }
 
     public function testStringConstructorWithFilesArray(): void
     {
         $filter = new RenameUploadMock($this->targetFile);
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertSame(
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertSame(
             [
                 'tmp_name' => $this->targetFile,
                 'name'     => $this->targetFile,
@@ -174,7 +157,7 @@ class RenameUploadTest extends TestCase
                 'name'     => $this->targetFile,
             ])
         );
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame('falsefile', $filter('falsefile'));
     }
 
     /**
@@ -182,59 +165,68 @@ class RenameUploadTest extends TestCase
      */
     public function testStringConstructorWithPsrFile(): void
     {
-        $sourceFile = $this->sourceFile;
-        $targetFile = $this->targetFile;
+        $originalStream = $this->createMock(StreamInterface::class);
+        $originalStream->expects(self::once())
+            ->method('getMetadata')
+            ->with('uri')
+            ->willReturn($this->sourceFile);
 
-        $originalStream = $this->prophesize(StreamInterface::class);
-        $originalStream->getMetadata('uri')->willReturn($this->sourceFile);
+        $originalFile = $this->createMock(UploadedFileInterface::class);
+        $originalFile->expects(self::once())
+            ->method('getStream')
+            ->willReturn($originalStream);
 
-        $originalFile = $this->prophesize(UploadedFileInterface::class);
-        $originalFile->getStream()->will(function ($args, $mock) use ($originalStream): object {
-            $mock->getStream()->willThrow(new RuntimeException('Cannot call getStream() more than once'));
-            return $originalStream->reveal();
-        });
-        $originalFile->getClientFilename()->willReturn($targetFile);
-        $originalFile
-            ->moveTo($targetFile)
-            ->will(function ($args) use ($sourceFile): void {
-                $targetFile = array_shift($args);
-                copy($sourceFile, $targetFile);
-            })
-            ->shouldBeCalled();
-        $originalFile->getClientMediaType()->willReturn(null);
+        $originalFile->expects(self::atLeast(1))
+            ->method('getClientFilename')
+            ->willReturn($this->targetFile);
 
-        $renamedStream = $this->prophesize(StreamInterface::class);
-        $streamFactory = $this->prophesize(StreamFactoryInterface::class);
-        $streamFactory
-            ->createStreamFromFile($targetFile)
-            ->will([$renamedStream, 'reveal']);
+        $originalFile->expects(self::once())
+            ->method('moveTo')
+            ->with(self::callback(function ($argument) {
+                self::assertSame($this->targetFile, $argument);
+                copy($this->sourceFile, $this->targetFile);
 
-        $renamedFile = $this->prophesize(UploadedFileInterface::class);
+                return true;
+            }));
 
-        $fileFactory = $this->prophesize(UploadedFileFactoryInterface::class);
-        $fileFactory
-            ->createUploadedFile(
-                Argument::that([$renamedStream, 'reveal']),
+        $originalFile->expects(self::once())
+            ->method('getClientMediaType')
+            ->willReturn(null);
+
+        $renamedStream = $this->createMock(StreamInterface::class);
+        $streamFactory = $this->createMock(StreamFactoryInterface::class);
+        $streamFactory->expects(self::once())
+            ->method('createStreamFromFile')
+            ->with($this->targetFile)
+            ->willReturn($renamedStream);
+
+        $renamedFile = $this->createMock(UploadedFileInterface::class);
+
+        $fileFactory = $this->createMock(UploadedFileFactoryInterface::class);
+        $fileFactory->expects(self::once())
+            ->method('createUploadedFile')
+            ->with(
+                $renamedStream,
                 0, // we can hardcode this, as we know the file is empty
                 UPLOAD_ERR_OK,
-                $targetFile,
+                $this->targetFile,
                 null
             )
-            ->will([$renamedFile, 'reveal']);
+            ->willReturn($renamedFile);
 
-        $filter = new RenameUploadMock($targetFile);
-        $this->assertSame($targetFile, $filter->getTarget());
+        $filter = new RenameUploadMock($this->targetFile);
+        self::assertSame($this->targetFile, $filter->getTarget());
 
-        $filter->setStreamFactory($streamFactory->reveal());
-        $filter->setUploadFileFactory($fileFactory->reveal());
+        $filter->setStreamFactory($streamFactory);
+        $filter->setUploadFileFactory($fileFactory);
 
-        $moved = $filter($originalFile->reveal());
+        $moved = $filter($originalFile);
 
-        $this->assertSame($renamedFile->reveal(), $moved);
+        self::assertSame($renamedFile, $moved);
 
-        $secondResult = $filter($originalFile->reveal());
+        $secondResult = $filter($originalFile);
 
-        $this->assertSame($moved, $secondResult);
+        self::assertSame($moved, $secondResult);
     }
 
     public function testArrayConstructorParam(): void
@@ -242,25 +234,25 @@ class RenameUploadTest extends TestCase
         $filter = new RenameUploadMock([
             'target' => $this->targetFile,
         ]);
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertSame($this->targetFile, $filter($this->sourceFile));
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertSame($this->targetFile, $filter($this->sourceFile));
+        self::assertSame('falsefile', $filter('falsefile'));
     }
 
     public function testConstructTruncatedTarget(): void
     {
         $filter = new FileRenameUpload('*');
-        $this->assertSame('*', $filter->getTarget());
-        $this->assertSame($this->sourceFile, $filter($this->sourceFile));
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame('*', $filter->getTarget());
+        self::assertSame($this->sourceFile, $filter($this->sourceFile));
+        self::assertSame('falsefile', $filter('falsefile'));
     }
 
     public function testTargetDirectory(): void
     {
         $filter = new RenameUploadMock($this->targetPath);
-        $this->assertSame($this->targetPath, $filter->getTarget());
-        $this->assertSame($this->targetPathFile, $filter($this->sourceFile));
-        $this->assertSame('falsefile', $filter('falsefile'));
+        self::assertSame($this->targetPath, $filter->getTarget());
+        self::assertSame($this->targetPathFile, $filter($this->sourceFile));
+        self::assertSame('falsefile', $filter('falsefile'));
     }
 
     public function testOverwriteWithExistingFile(): void
@@ -272,8 +264,8 @@ class RenameUploadTest extends TestCase
 
         copy($this->sourceFile, $this->targetFile);
 
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertSame($this->targetFile, $filter($this->sourceFile));
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertSame($this->targetFile, $filter($this->sourceFile));
     }
 
     public function testCannotOverwriteExistingFile(): void
@@ -285,11 +277,11 @@ class RenameUploadTest extends TestCase
 
         copy($this->sourceFile, $this->targetFile);
 
-        $this->assertSame($this->targetFile, $filter->getTarget());
-        $this->assertFalse($filter->getOverwrite());
+        self::assertSame($this->targetFile, $filter->getTarget());
+        self::assertFalse($filter->getOverwrite());
         $this->expectException(Exception\InvalidArgumentException::class);
         $this->expectExceptionMessage('already exists');
-        $this->assertSame($this->targetFile, $filter($this->sourceFile));
+        self::assertSame($this->targetFile, $filter($this->sourceFile));
     }
 
     public function testGetRandomizedFile(): void
@@ -300,7 +292,7 @@ class RenameUploadTest extends TestCase
             'randomize' => true,
         ]);
 
-        $this->assertMatchesRegularExpression(
+        self::assertMatchesRegularExpression(
             '#' . str_replace('\\', '\\\\', $fileNoExt) . '_.{23}\.xml#',
             $filter($this->sourceFile)
         );
@@ -317,7 +309,7 @@ class RenameUploadTest extends TestCase
 
         $oldFilePathInfo = pathinfo($this->sourceFile);
 
-        $this->assertMatchesRegularExpression(
+        self::assertMatchesRegularExpression(
             '#' . str_replace('\\', '\\\\', $fileNoExt) . '.' . $oldFilePathInfo['extension'] . '#',
             $filter($this->sourceFile)
         );
@@ -334,7 +326,7 @@ class RenameUploadTest extends TestCase
 
         $oldFilePathInfo = pathinfo($this->sourceFile);
 
-        $this->assertMatchesRegularExpression(
+        self::assertMatchesRegularExpression(
             '#' . str_replace('\\', '\\\\', $fileNoExt) . '_.{23}\.' . $oldFilePathInfo['extension'] . '#',
             $filter($this->sourceFile)
         );
@@ -348,7 +340,7 @@ class RenameUploadTest extends TestCase
             'randomize' => true,
         ]);
 
-        $this->assertMatchesRegularExpression(
+        self::assertMatchesRegularExpression(
             '#' . str_replace('\\', '\\\\', $fileNoExt) . '_.{13}#',
             $filter($this->sourceFile)
         );
@@ -358,7 +350,7 @@ class RenameUploadTest extends TestCase
     {
         $this->expectException(Exception\InvalidArgumentException::class);
         $this->expectExceptionMessage('Invalid target');
-        $filter = new FileRenameUpload(1234);
+        new FileRenameUpload(1234);
     }
 
     public function testCanFilterMultipleTimesWithSameResult(): void
@@ -370,21 +362,22 @@ class RenameUploadTest extends TestCase
 
         $firstResult = $filter($this->sourceFile);
 
-        $this->assertStringContainsString('newfile', $firstResult);
+        self::assertStringContainsString('newfile', $firstResult);
 
         $secondResult = $filter($this->sourceFile);
 
-        $this->assertSame($firstResult, $secondResult);
+        self::assertSame($firstResult, $secondResult);
     }
 
-    public function returnUnfilteredDataProvider()
+    /** @return list<array{0:mixed|null}> */
+    public function returnUnfilteredDataProvider(): array
     {
         return [
             [null],
             [new stdClass()],
             [
                 [
-                    $this->sourceFile,
+                    '/some-file',
                     'something invalid',
                 ],
             ],
@@ -394,14 +387,14 @@ class RenameUploadTest extends TestCase
     /**
      * @dataProvider returnUnfilteredDataProvider
      */
-    public function testReturnUnfiltered($input): void
+    public function testReturnUnfiltered(mixed $input): void
     {
         $filter = new RenameUploadMock([
             'target'    => $this->targetFile,
             'randomize' => true,
         ]);
 
-        $this->assertSame($input, $filter($input));
+        self::assertSame($input, $filter($input));
     }
 
     /**
@@ -429,8 +422,8 @@ class RenameUploadTest extends TestCase
         ];
 
         // Check the result twice for the `alreadyFiltered` cache path
-        $this->assertSame($sapiTarget, $filter($sapiSource));
-        $this->assertSame($sapiTarget, $filter($sapiSource));
+        self::assertSame($sapiTarget, $filter($sapiSource));
+        self::assertSame($sapiTarget, $filter($sapiSource));
     }
 
     /**
@@ -445,6 +438,6 @@ class RenameUploadTest extends TestCase
             'name'     => basename($this->targetFile),
         ];
 
-        $this->assertSame($source, $filter($source));
+        self::assertSame($source, $filter($source));
     }
 }
