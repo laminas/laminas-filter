ac9fe186b10fdd1500fe19de7df4214f54558692
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2023-11-03 13:11:19 +0100

    review
    Signed-off-by: Stéphane Demonchaux <demonchaux.stephane@gmail.com>

--- test/Word/CamelCaseToSeparatorNoPcreUnicodeTest.php
+++ test/Word/CamelCaseToSeparatorNoPcreUnicodeTest.php
@@ -6,9 +6,6 @@ namespace LaminasTest\Filter\Word;
 
 use Laminas\Stdlib\StringUtils;
 use ReflectionClass;
-use ReflectionProperty;
-
-use const PHP_VERSION_ID;
 
 /**
  * Test class for Laminas\Filter\Word\CamelCaseToSeparator which simulates the
@@ -22,25 +19,13 @@ class CamelCaseToSeparatorNoPcreUnicodeTest extends CamelCaseToSeparatorTest
             self::markTestSkipped('PCRE is not compiled with Unicode support');
         }
 
-        if (PHP_VERSION_ID >= 80300) {
-            $reflectionClass = new ReflectionClass(StringUtils::class);
-            $reflectionClass->setStaticPropertyValue('hasPcreUnicodeSupport', false);
-            return;
-        }
-
-        $reflection = new ReflectionProperty(StringUtils::class, 'hasPcreUnicodeSupport');
-        $reflection->setValue(false);
+        $reflectionClass = new ReflectionClass(StringUtils::class);
+        $reflectionClass->setStaticPropertyValue('hasPcreUnicodeSupport', false);
     }
 
     public function tearDown(): void
     {
-        if (PHP_VERSION_ID >= 80300) {
-            $reflectionClass = new ReflectionClass(StringUtils::class);
-            $reflectionClass->setStaticPropertyValue('hasPcreUnicodeSupport', true);
-            return;
-        }
-
-        $reflection = new ReflectionProperty(StringUtils::class, 'hasPcreUnicodeSupport');
-        $reflection->setValue(true);
+        $reflectionClass = new ReflectionClass(StringUtils::class);
+        $reflectionClass->setStaticPropertyValue('hasPcreUnicodeSupport', true);
     }
 }
