b2dfcfabf859131c536e55cbc97cec1f11550bc2
George Steel <george@net-glue.co.uk>
2023-11-20 18:04:27 +0000

    Make the `list` option mandatory and verify return type of `T|null`
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/AllowListTest.php
+++ test/AllowListTest.php
@@ -35,7 +35,7 @@ class AllowListTest extends TestCase
 
     public function testConstructorDefaults(): void
     {
-        $filter = new AllowListFilter();
+        $filter = new AllowListFilter(['list' => ['a', 'b', 'c']]);
         self::assertNull($filter->filter('anything'));
     }
 
@@ -82,7 +82,7 @@ class AllowListTest extends TestCase
     #[DataProvider('defaultTestProvider')]
     public function testFilterWillReturnNullForAnyValueWhenNoListHasBeenSupplied(mixed $value): void
     {
-        $filter = new AllowListFilter();
+        $filter = new AllowListFilter(['list' => []]);
         self::assertNull($filter->filter($value));
     }
 
