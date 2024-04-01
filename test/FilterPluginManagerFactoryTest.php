e578411142efa7b0a53e5d12059381f30e7fbd87
George Steel <george@net-glue.co.uk>
2024-04-01 14:21:04 +0100

    Simplify the plugin manager factory
    FactoryInterface and SMv2 affordances are no longer relevant
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/FilterPluginManagerFactoryTest.php
+++ test/FilterPluginManagerFactoryTest.php
@@ -8,14 +8,10 @@ use Laminas\Filter\Boolean;
 use Laminas\Filter\FilterInterface;
 use Laminas\Filter\FilterPluginManager;
 use Laminas\Filter\FilterPluginManagerFactory;
-use Laminas\ServiceManager\ServiceLocatorInterface;
-use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use ReflectionObject;
-
-use function method_exists;
 
 class FilterPluginManagerFactoryTest extends TestCase
 {
@@ -23,52 +19,12 @@ class FilterPluginManagerFactoryTest extends TestCase
     {
         $container = $this->createMock(ContainerInterface::class);
         $factory   = new FilterPluginManagerFactory();
-
-        $filters = $factory($container, FilterPluginManagerFactory::class);
+        $filters   = $factory($container);
         self::assertInstanceOf(FilterPluginManager::class, $filters);
 
-        if (method_exists($filters, 'configure')) {
-            // laminas-servicemanager v3
-            $r = new ReflectionObject($filters);
-            $p = $r->getProperty('creationContext');
-            self::assertSame($container, $p->getValue($filters));
-        } else {
-            // laminas-servicemanager v2
-            self::assertSame($container, $filters->getServiceLocator());
-        }
-    }
-
-    #[Depends('testFactoryReturnsPluginManager')]
-    public function testFactoryConfiguresPluginManagerUnderContainerInterop(): void
-    {
-        $container = $this->createMock(ContainerInterface::class);
-        $filter    = static fn($value) => $value;
-
-        $factory = new FilterPluginManagerFactory();
-        $filters = $factory($container, FilterPluginManagerFactory::class, [
-            'services' => [
-                'test' => $filter,
-            ],
-        ]);
-        self::assertSame($filter, $filters->get('test'));
-    }
-
-    #[Depends('testFactoryReturnsPluginManager')]
-    public function testFactoryConfiguresPluginManagerUnderServiceManagerV2()
-    {
-        $container = $this->createMock(ServiceLocatorInterface::class);
-
-        $filter = static fn($value) => $value;
-
-        $factory = new FilterPluginManagerFactory();
-        $factory->setCreationOptions([
-            'services' => [
-                'test' => $filter,
-            ],
-        ]);
-
-        $filters = $factory->createService($container);
-        self::assertSame($filter, $filters->get('test'));
+        $r = new ReflectionObject($filters);
+        $p = $r->getProperty('creationContext');
+        self::assertSame($container, $p->getValue($filters));
     }
 
     public function testConfiguresFilterServicesWhenFound(): void
@@ -80,12 +36,12 @@ class FilterPluginManagerFactoryTest extends TestCase
                     'test' => Boolean::class,
                 ],
                 'factories' => [
-                    'test-too' => static fn($container): MockObject => $filter,
+                    'test-too' => static fn(): MockObject&FilterInterface => $filter,
                 ],
             ],
         ];
 
-        $container = $this->createMock(ServiceLocatorInterface::class);
+        $container = $this->createMock(ContainerInterface::class);
         $container->expects(self::atLeast(2))
             ->method('has')
             ->willReturnMap([
@@ -98,7 +54,7 @@ class FilterPluginManagerFactoryTest extends TestCase
             ->willReturn($config);
 
         $factory = new FilterPluginManagerFactory();
-        $filters = $factory($container, 'FilterManager');
+        $filters = $factory($container);
 
         self::assertInstanceOf(FilterPluginManager::class, $filters);
         self::assertTrue($filters->has('test'));
@@ -109,7 +65,7 @@ class FilterPluginManagerFactoryTest extends TestCase
 
     public function testDoesNotConfigureFilterServicesWhenServiceListenerPresent(): void
     {
-        $container = $this->createMock(ServiceLocatorInterface::class);
+        $container = $this->createMock(ContainerInterface::class);
 
         $container->expects(self::once())
             ->method('has')
@@ -117,7 +73,7 @@ class FilterPluginManagerFactoryTest extends TestCase
             ->willReturn(true);
 
         $factory = new FilterPluginManagerFactory();
-        $filters = $factory($container, 'FilterManager');
+        $filters = $factory($container);
 
         self::assertInstanceOf(FilterPluginManager::class, $filters);
         self::assertFalse($filters->has('test'));
@@ -126,7 +82,7 @@ class FilterPluginManagerFactoryTest extends TestCase
 
     public function testDoesNotConfigureFilterServicesWhenConfigServiceNotPresent(): void
     {
-        $container = $this->createMock(ServiceLocatorInterface::class);
+        $container = $this->createMock(ContainerInterface::class);
 
         $container->expects(self::exactly(2))
             ->method('has')
@@ -138,14 +94,14 @@ class FilterPluginManagerFactoryTest extends TestCase
         $container->expects(self::never())->method('get');
 
         $factory = new FilterPluginManagerFactory();
-        $filters = $factory($container, 'FilterManager');
+        $filters = $factory($container);
 
         self::assertInstanceOf(FilterPluginManager::class, $filters);
     }
 
     public function testDoesNotConfigureFilterServicesWhenConfigServiceDoesNotContainFiltersConfig(): void
     {
-        $container = $this->createMock(ServiceLocatorInterface::class);
+        $container = $this->createMock(ContainerInterface::class);
 
         $container->expects(self::exactly(2))
             ->method('has')
@@ -160,7 +116,7 @@ class FilterPluginManagerFactoryTest extends TestCase
             ->willReturn(['foo' => 'bar']);
 
         $factory = new FilterPluginManagerFactory();
-        $filters = $factory($container, 'FilterManager');
+        $filters = $factory($container);
 
         self::assertInstanceOf(FilterPluginManager::class, $filters);
         self::assertFalse($filters->has('foo'));
