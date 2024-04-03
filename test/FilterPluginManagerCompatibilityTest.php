4ce4f1fe4c09e73abaf57f147c7cb8b7a6e0dc5f
George Steel <george@net-glue.co.uk>
2024-04-04 00:39:01 +0100

    Re-work the compatibility test copying tests from the trait shipped in service manager.
    The test trait shipped on service manager cannot be used because it is designed for `SingleInstancePluginManager` which `FilterPluginManager` cannot inherit from.
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/FilterPluginManagerCompatibilityTest.php
+++ test/FilterPluginManagerCompatibilityTest.php
@@ -5,53 +5,77 @@ declare(strict_types=1);
 namespace LaminasTest\Filter;
 
 use Generator;
-use Laminas\Filter\Exception\RuntimeException;
-use Laminas\Filter\FilterInterface;
+use Laminas\Filter\ConfigProvider;
 use Laminas\Filter\FilterPluginManager;
-use Laminas\ServiceManager\ServiceManager;
-use Laminas\ServiceManager\Test\CommonPluginManagerTrait;
+use Laminas\ServiceManager\Exception\InvalidServiceException;
 use PHPUnit\Framework\TestCase;
-use ReflectionProperty;
+use stdClass;
+use Throwable;
 
+use function assert;
+use function class_exists;
 use function strpos;
 
 class FilterPluginManagerCompatibilityTest extends TestCase
 {
-    use CommonPluginManagerTrait;
-
     protected static function getPluginManager(): FilterPluginManager
     {
-        return new FilterPluginManager(new ServiceManager());
+        return CreatePluginManager::withDefaults();
     }
 
-    protected function getV2InvalidPluginException(): string
-    {
-        return RuntimeException::class;
-    }
-
-    protected function getInstanceOf(): string
-    {
-        return FilterInterface::class;
-    }
-
-    /** @return Generator<string, array{0: string, 1: string}> */
+    /** @return Generator<string, array{0: string, 1: class-string}> */
     public static function aliasProvider(): Generator
     {
-        $pluginManager = self::getPluginManager();
-        $r             = new ReflectionProperty($pluginManager, 'aliases');
-        $aliases       = $r->getValue($pluginManager);
-        self::assertIsArray($aliases);
+        $aliases = (new ConfigProvider())()['filters']['aliases'] ?? [];
 
         foreach ($aliases as $alias => $target) {
-            self::assertIsString($alias);
-            self::assertIsString($target);
-
             // Skipping as it has required options
             if (strpos($target, 'DataUnitFormatter') !== false) {
                 continue;
             }
 
+            assert(class_exists($target));
+
             yield $alias => [$alias, $target];
         }
+    }
+
+    /**
+     * @param class-string $expected
+     * @dataProvider aliasProvider
+     */
+    public function testPluginAliasesResolve(string $alias, string $expected): void
+    {
+        self::assertInstanceOf(
+            $expected,
+            self::getPluginManager()->get($alias),
+            "Alias '$alias' does not resolve'",
+        );
+    }
+
+    public function testLoadingInvalidElementRaisesException(): void
+    {
+        $manager = self::getPluginManager();
+        $manager->configure([
+            'factories' => [
+                'test' => static fn(): stdClass => new stdClass(),
+            ],
+        ]);
+        $this->expectException($this->getServiceNotFoundException());
+        $manager->get('test');
+    }
+
+    /** @return class-string<Throwable> */
+    protected function getServiceNotFoundException(): string
+    {
+        return InvalidServiceException::class;
+    }
+
+    public function testRegisteringInvalidElementRaisesException(): void
+    {
+        $manager = self::getPluginManager();
+        $this->expectException($this->getServiceNotFoundException());
+        /** @psalm-suppress InvalidArgument - Because we are testing an invalid argument */
+        $manager->setService('test', new stdClass());
     }
 }
